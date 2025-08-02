4e92d1c4ef992100d5e72d39f21538c778b39b89
nikhil <nikhilrajpurohitnkp@gmail.com>
2025-08-03 02:28:43 +0530

    added auth routes

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -3,6 +3,8 @@ import {z} from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache'; //updating the data displayed in the invoices route,hense clearing cache and triggering new request to  server.
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -93,4 +95,23 @@ export  async function deleteInvoices(id:string){
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");//no redirect as we are in the main page itself
 
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
